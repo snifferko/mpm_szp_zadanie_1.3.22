a55b9c8e79150046b24c875262254ae44417d090
Martin Kocun <mkocun@gmail.com>
2017-05-01 09:02:46 +0200

    rozsirenie o chybovy return

--- riesenie.c
+++ riesenie.c
@@ -2,8 +2,11 @@
 	author: Martin Kocun
 	zadanie: 1.3.22
 */
-
 #define _GNU_SOURCE
+#define CHYBA -1
+#define MIN_N 1
+#define MIN_L 2
+#define OK 0
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -57,7 +60,7 @@ char *split_string(char *s, unsigned int n)
 	return res;
 }
 
-void nahodne_prepletene_slovo(char *w, unsigned int l, unsigned int n)
+char nahodne_prepletene_slovo(char *w, unsigned int l, unsigned int n)
 {
 	char *my_alphabet;
 	char *shuffled_alphabet;
@@ -71,60 +74,61 @@ void nahodne_prepletene_slovo(char *w, unsigned int l, unsigned int n)
 	int j;
 	int k;
 	int num;
-	
-	
-	my_alphabet=split_string(A,n);
-	shuffled_alphabet=strfry(strdup(my_alphabet));
 
-	if(l==0 || n==0) {
-		w[0]=0;
-	} else if(l==1) {
-		w[0]=my_alphabet[rand()%n];
-		w[1]=0;
+	if(n < MIN_N || l < MIN_L) {
+		return CHYBA;
 		
-	} else if(n==1) {
-		for(int i=0;i<l;i++){
-			w[i]=my_alphabet[0];
-		}
-		w[l]=0;
 	} else {
-		types_num=n<l ? n/2 : l/2;
-		type=rand()%types_num+1;
 
-		chain_alphabet=split_string(shuffled_alphabet,type);
-		chain=get_chain(chain_alphabet);
-		
-		chain_len=strlen(chain);
-		random_num=l-chain_len;
-		i=0;
-		
-		for(j=0;j<chain_len-2;j++){
-			w[i]=chain[j];
-			i++;	
-			if(random_num>0){
-				num=rand()%(random_num+1);
-				for(k=0;k<num;k++){
-					w[i]=my_alphabet[rand()%n];
-					i++;
-				}
-				random_num-=num;	
+		my_alphabet=split_string(A,n);
+		shuffled_alphabet=strfry(strdup(my_alphabet));
+
+		if(n==1) {
+			for(int i=0;i<l;i++){
+				w[i]=my_alphabet[0];
 			}
-		}
-		w[i]=chain[chain_len-2];
-		i++;
-		for(j=0;j<random_num;j++){
-			w[i]=my_alphabet[rand()%n];
-			i++;
-		}
-		w[l-1]=chain[chain_len-1];
-		w[l]=0;
+			w[l]=0; 
+		} else {
+			types_num=n<l ? n/2 : l/2;
+			type=rand()%types_num+1;
 
-		free(chain_alphabet);
-		free(chain);
+			chain_alphabet=split_string(shuffled_alphabet,type);
+			chain=get_chain(chain_alphabet);
+			
+			chain_len=strlen(chain);
+			random_num=l-chain_len;
+			i=0;
+			
+			for(j=0;j<chain_len-2;j++){
+				w[i]=chain[j];
+				i++;	
+				if(random_num>0){
+					num=rand()%(random_num+1);
+					for(k=0;k<num;k++){
+						w[i]=my_alphabet[rand()%n];
+						i++;
+					}
+					random_num-=num;	
+				}
+			}
+			w[i]=chain[chain_len-2];
+			i++;
+			for(j=0;j<random_num;j++){
+				w[i]=my_alphabet[rand()%n];
+				i++;
+			}
+			w[l-1]=chain[chain_len-1];
+			w[l]=0;
+
+			free(chain_alphabet);
+			free(chain);
+		}
+
+		free(my_alphabet);
+		free(shuffled_alphabet);
 	}
 
-	free(my_alphabet);
-	free(shuffled_alphabet);
+	return OK;
 }
 
 
@@ -134,11 +138,14 @@ int main()
 	char *w;
 	
 	srand(time(NULL));
-	l=rand()%20+1;
+	l=rand()%20+2;
 	n=rand()%strlen(A)+1;
 	w=malloc((l+1)*sizeof(char));
-	nahodne_prepletene_slovo(w,l,n);
-	printf("l:%d, n:%d, w:%s\n",l,n,w);
+	if(nahodne_prepletene_slovo(w,l,n)==OK){
+		printf("l:%d, n:%d, w:%s\n",l,n,w);
+	} else {
+		printf("Nastala chyba");
+	}
 	free(w);
 
 
